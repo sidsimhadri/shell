647b874ffcd829548fe44f949b45cfa2679d9a9e
sidsimhadri <sidsimhadri@gmail.com>
2022-03-03 15:56:30 -0500

    gradescope

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@
 #include "tokenizer.h"
 
 #define max 255
-int shell(vect_t *args, vect_t *last, char *infile, char *outfile);
+int shell(vect_t *args, vect_t *colon, vect_t *last, char *infile, char *outfile);
 void processargs(vect_t *args, vect_t *last_args, char *in, char *out);
 
 // executes the vector of arguments
@@ -52,7 +52,7 @@ int source (vect_t *args) {
            vect_set(read, i, strcspn(vect_get(read, i),"\n" ));
            i++;
            }
-	    shell(args, tokenize(last), NULL, NULL);
+	    shell(args, NULL, tokenize(last), NULL, NULL);
     }
 
     fclose(f);
@@ -60,7 +60,7 @@ int source (vect_t *args) {
 }
 
 int prev (vect_t *args, vect_t *last) {
-  shell(last, last, NULL, NULL);
+  shell(last, NULL, last, NULL, NULL);
 }
 
 
@@ -78,24 +78,9 @@ printf("cd\n"
 }
 
 // run the shell's child processes
-int shell(vect_t *args, vect_t *last, char *infile, char *outfile) {
+int shell(vect_t *args, vect_t *args2, vect_t *last, char *infile, char *outfile) {
   // if we want to exit the shell then do
-  if(strcmp(vect_get(args, 0), "exit") == 0) {
-    printf("Bye bye.\n");
-    exit(0);
-  }
-  if(strcmp(vect_get(args, 0), "cd") == 0) {
-     return cd(args);
-  }
-  if(strcmp(vect_get(args, 0), "source") == 0) {
-    source(args);
-  }
-  if(strcmp(vect_get(args, 0), "prev") == 0) {
-    return prev(args, last);
-  }
-  if(strcmp(vect_get(args, 0), "help") == 0) {
-    help(args);
-  }
+
   int cpid = fork(); // fork
   int in;
   int out;
@@ -133,101 +118,51 @@ int shell(vect_t *args, vect_t *last, char *infile, char *outfile) {
   } else {
     int status;
     waitpid(cpid, &status, 0);
+    if(args2) {
+    processargs(args2, args, NULL, NULL);
+    }
   }
 }
 
-
-int colon(vect_t *args1, vect_t *args2, vect_t *last, char *infile, char *outfile) {
-       pid_t a, b;
-       a = fork();
-      int cpid = fork(); // fork
-      int in;
-      int out;
-      if(a == 0) {
-        if(infile) {
-            // Close standard in
-            if (close(0) == -1) {
-              perror("Error closing stdin");
-              exit(1);
-            }
-
-
-            // Open the file for reading
-            in = open(infile, O_RDONLY);
-
-            // The open file should replace standard in
-            assert(in == 0);
-        }
-        if(outfile) {
-            // Close standard out
-            if (close(1) == -1) {
-              perror("Error closing stdout");
-              exit(1);
-            }
-
-            // Create the file, truncate it if it exists
-            out = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-            // The open file should replace standard out
-            assert(out == 1);
-        }
-        //i switched execute and waitpid's location
-        execute(args1); // execute the arguments
-                             // wait on the status to finish
-      } else {
-        int statusa, statusb;
-        waitpid(a, &statusa, 0);
-        b = fork();
-        if(b == 0) {
-            processargs(args2, args1, NULL, NULL);
-        }
-        waitpid(b, &statusb, 0);
-      }
-}
-
 int piper(vect_t *args1, vect_t *args2, vect_t *last, char *in, char *out) {
+    int pipe_fds[2]; // the pipe system call creates two file descriptors in the 2-element
+                     // array given as argument
 
-    pid_t a, b;
+    assert(pipe(pipe_fds) == 0); // returns 0 on success
 
-    a = fork();
+    int read_fd = pipe_fds[0]; // index 0 is the "read end" of the pipe
+    int write_fd = pipe_fds[1]; // index 1 is the "write end" of the pipt
 
-    if (a == 0) {
-        int pipe_fds[2]; // the pipe system call creates two file descriptors in the 2-element
-                         // array given as argument
+    int child_pid = fork();
 
-        assert(pipe(pipe_fds) == 0); // returns 0 on success
+    if (child_pid > 0) { // in parent
+      close(read_fd); // close the other end of the pipe
 
-        int read_fd = pipe_fds[0]; // index 0 is the "read end" of the pipe
-        int write_fd = pipe_fds[1]; // index 1 is the "write end" of the pipe
-        b = fork();
-        if (b == 0) {
+      // replace stdout with the write end of the pipe
+      if (close(1) == -1) {
+        perror("Error closing stdout");
+        exit(1);
+      }
+      assert(dup(write_fd) == 1);
 
-              close(read_fd); // close the other end of the pipe
+      execute(args1);
+    }
+    else if (child_pid == 0) { // in child
+      close(write_fd); // close the other end of the pipe
 
-              // replace stdout with the write end of the pipe
-              if (close(1) == -1) {
-                perror("Error closing stdout");
-                exit(1);
-              }
-            execute(args1);
-        }
-              close(write_fd); // close the other end of the pipe
+      // replace stdin with the read end of the pipe
+      if ( close(0) == -1) {
+        perror("Error closing stdin");
+        exit(1);
+      }
+      assert(dup(read_fd) == 0);
 
-              // replace stdin with the read end of the pipe
-              if ( close(0) == -1) {
-                perror("Error closing stdin");
-                exit(1);
-              }
-              execute(args2);
-              int status1;
-              waitpid(b, &status1, 0);
-
-        }
-
-        else {
-            int status2;
-            waitpid(a, &status2, 0);
-        }
+      execute(args2);
+    }
+    else {
+      perror("Error - fork failed");
+      exit(1);
+    }
 
 }
 // handles semicolons, etc
@@ -246,7 +181,7 @@ void processargs(vect_t *args, vect_t *last_args, char *in, char *out) {
         }
 	   if(strcmp(vect_get(args, i), ";") == 0) { // if the thing at i is a semi colon
          vect_add(tmp, vect_get(args, i));
-         colon(tmp, tmp2, last_args, in, out);
+         shell(tmp, tmp2, last_args, in, out);
          tmp = NULL;
        }
         else {
@@ -264,7 +199,7 @@ void processargs(vect_t *args, vect_t *last_args, char *in, char *out) {
 
 	 }
      if(tmp != NULL) {
-          shell(args, last_args, in, out);
+          shell(args, NULL, last_args, in, out);
      }
 
 }
@@ -277,19 +212,37 @@ int main(int argc, char **argv) {
       printf("Welcome to mini-shell.\n");
       while(1) { // while true
          printf("shell $ "); // print shell prompt
-         char* line = fgets(commands, max, stdin); // get input from stdin
-         if(!line) { // if we dont have a line then we're done
+          // get input from stdin
+         if(fgets(commands, max, stdin) == NULL) { // if we dont have a line then we're done
            printf("\nBye bye.\n"); // print bye bye on a new line
            exit(0); // exit
          }
          args = tokenize(commands); // tokenize the commands
-//         int i = 0;
+           if(strcmp(vect_get(args, 0), "exit") == 0) {
+             printf("Bye bye.\n");
+             exit(0);
+           }
+           else if(strcmp(vect_get(args, 0), "cd") == 0) {
+              return cd(args);
+           }
+           else if(strcmp(vect_get(args, 0), "source") == 0) {
+             return source(args);
+           }
+           else if(strcmp(vect_get(args, 0), "prev") == 0) {
+             return prev(args, NULL);
+           }
+           else if(strcmp(vect_get(args, 0), "help") == 0) {
+             return help(args);
+           }
+           else {
+         int i = 0;
 //          while (i < vect_size(args)) {
 //             vect_set(args, i, strcspn(vect_get(args, i),"\n" ));
 //             i++;
 //             }
 
 	      processargs(args, last_args, NULL, NULL);
+	      }
       }
     }
     return 0;
